d0d73e49b196cdb18ed19109a22a6dbdab2a3191
Vincent Nys <vincentnys@gmail.com>
2020-01-09 08:33:18 +0100

    Eerste paar oplossingen...

---
+++ controlestrategieën.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE ModernWays;
+USE ModernWays;
+CREATE TABLE Cursussen (
+Titel VARCHAR(100) CHAR SET UTF8MB4 NOT NULL
+);
+INSERT INTO CURSUSSEN (Titel)
+VALUES
+('Programmeren'),
+('Databanken'),
+('Webtechnologie');
+`
